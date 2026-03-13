12737b89f147adcc4c1671b2a5315e52a6561d85
Ankit Das <ankit10869734@gmail.com>
2026-03-13 15:36:59 +0530

    Delete complainv2 directory

--- complainv2/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # This shows landing OR dashboard based on login
-    path('signup/', views.signup_view, name='signup'), 
-    path('dashboard/', views.dashboard, name='dashboard'),
-]
